af8053a019b5890993842b99be4ac092a210e770
Jai Agrawal <jaiagrawal1110@gmail.com>
2026-07-31 01:08:32 +0530

    Time: 1 ms (100%), Space: 44 MB (62.41%) - LeetHub

---
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        int n=s.length();
+        k%=n;
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<n;i++){
+            ans.append(s.charAt((i+k)%n));
+        }
+        return ans.toString();
+        
+    }
+}
