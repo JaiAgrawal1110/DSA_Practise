790caf0a4379b22ec5b881c228b51dc29a235fd5
Jai Agrawal <jaiagrawal1110@gmail.com>
2026-07-30 20:37:07 +0530

    Time: 1 ms (98.13%), Space: 43.6 MB (50.36%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int n=num.length();
+        int even=0;
+        int odd=0;
+
+        for(int i=0;i<n;i++){
+            int digit=num.charAt(i)-'0';
+            if(i%2==0){
+                even+=digit;
+            }else{
+                odd+=digit;
+            }
+        }
+        return odd==even;
+    }
+}
